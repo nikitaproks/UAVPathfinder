92d0d562f233f054daf01226a40589d2756930db
FaroukHai <60575260+FaroukHai@users.noreply.github.com>
2023-06-03 17:55:57 +0200

    transformed city into cartesian coordinates

--- UAVpathfinder/maps/map_test.py
+++ UAVpathfinder/maps/map_test.py
@@ -1,4 +1,20 @@
 from map import Map
+from utils import coord_to_cart
+from utils import haversine_distance
+
+print(
+    haversine_distance(
+        [48.15454749016991, 11.544871334811818], [48.15633324537993, 11.545783285821432]
+    )
+)
+
+print(
+    coord_to_cart(
+        [48.15454749016991, 11.544871334811818], [48.15633324537993, 11.545783285821432]
+    )
+    + [0]
+)
+
 
 new_map = Map(
     start_coord=[48.15454749016991, 11.544871334811818],
