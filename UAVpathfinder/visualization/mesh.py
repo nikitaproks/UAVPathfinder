23171f7de8d006ad2a2e314a405a2e4ace0160c5
Mykyta Prokaiev <mykytaprokaiev@gmail.com>
2023-06-04 13:07:33 +0200

    optimized nice code

--- UAVpathfinder/visualization/mesh.py
+++ UAVpathfinder/visualization/mesh.py
@@ -1,14 +1,15 @@
 from typing import Union
 import numpy as np
 from tqdm import tqdm
-
-
-from UAVpathfinder.maps.map import Map
+import trimesh
+from shapely.geometry import Polygon
+from UAVpathfinder.maps.map import Map, Building
 from UAVpathfinder.visualization.render import Render3D
 from UAVpathfinder.visualization.dependencies import (
-    rotate_points,
+    get_position_rotation_height,
     PlanarPolygon,
 )
+from UAVpathfinder.maps.free_space_graph import FreeSpaceGraph
 
 
 class Mesh:
@@ -59,69 +60,61 @@ class Mesh:
             (x.flatten(), y.flatten(), Z.flatten())
         )
 
-    def create_cylinder(
+    def _create_cylinder(
         self,
         start_point: list[int],
         end_point: list[int],
         radius: float,
-        resolution: float = 0.1,
     ):
         """Create a cylinder mesh between two points."""
 
-        # Create empty arrays to store points and faces
-        body_points = np.empty((0, 3), dtype=np.float64)
-
-        # Convert start and end points to numpy arrays
         start_point = np.array(start_point, dtype=np.float64)
         end_point = np.array(end_point, dtype=np.float64)
 
-        # Calculate the height and axis vector of the cylinder
-        height = np.linalg.norm(end_point - start_point)
-        axis_vector = (end_point - start_point) / height
+        position: np.ndarray
+        rotation: np.ndarray
+        height: float
 
-        shell_points = self._create_cylinder_shell_points(
-            height, radius, resolution
+        position, rotation, height = get_position_rotation_height(
+            start_point, end_point
         )
 
-        # Generate a grid of points within the bounding box of the circle
-        bottom_points = self._create_circle_points(
-            [0, 0, 0], radius, resolution
+        # Create the transformation matrix
+        transform = np.eye(4)
+        transform[:3, 3] = position
+        transform[:3, :3] = rotation[:3, :3]
+
+        cylinder_mesh = trimesh.creation.cylinder(
+            radius=radius, height=height, transform=transform
         )
-        top_points = self._create_circle_points(
-            [0, 0, height], radius, resolution
+        return cylinder_mesh
+
+    def add_path(self, path: list[list[int]], radius: float):
+        """Add a path to the mesh."""
+        for i in range(len(path) - 2):
+            cylinder_mesh = self._create_cylinder(
+                path[i], path[i + 1], radius
+            )
+            self._add_triangles(cylinder_mesh.triangles)
+
+    def add_box(
+        self,
+        polygon: Polygon,
+        height: float,
+    ):
+        """Add a box to the mesh."""
+        polygon_mesh = trimesh.creation.extrude_polygon(
+            polygon, height
         )
+        self._add_triangles(polygon_mesh.triangles)
 
-        # Add the points to the body points
-        body_points = np.vstack(
-            (body_points, shell_points, top_points, bottom_points)
-        )
-
-        # Rotate the points to align with the axis vector
-        body_points = rotate_points(body_points, axis_vector)
-
-        points: np.ndarray = body_points
-        mesh.add_vertices(points)
-        return points
-
-    def add_2d_polygon(self, vertices: list[list[float]]):
-        """Add a 2D polygon to the mesh."""
-        polygon = PlanarPolygon(vertices)
-        if not polygon.is_valid:
-            return
-        self.polygon = polygon
-        self.add(polygon.vertices_3d, polygon.triangles_3d)
-
-    def add_vertices(self, vertices: np.ndarray):
+    def _add_vertices(self, vertices: np.ndarray):
         self.vertices = np.vstack((self.vertices, vertices))
 
-    def add_triangles(self, triangles: np.ndarray):
+    def _add_triangles(self, triangles: np.ndarray):
         self.triangles = np.vstack((self.triangles, triangles))
 
-    def add(self, vertices: np.ndarray, triangles: np.ndarray):
-        self.add_triangles(triangles)
-        self.add_vertices(vertices)
-
-    def get_data(self):
+    def get_vertices_faces(self):
         vertices = np.unique(self.triangles.reshape(-1, 3), axis=0)
         _, faces = np.unique(
             self.triangles.reshape(-1, 3), axis=0, return_inverse=True
@@ -132,12 +125,13 @@ class Mesh:
         return vertices, all_faces
 
     def get_bounding_box(self):
-        min_x = np.min(self.vertices[:, 0])
-        min_y = np.min(self.vertices[:, 1])
-        min_z = np.min(self.vertices[:, 2])
-        max_x = np.max(self.vertices[:, 0])
-        max_y = np.max(self.vertices[:, 1])
-        max_z = np.max(self.vertices[:, 2])
+        vertices, faces = self.get_vertices_faces()
+        min_x = np.min(vertices[:, 0])
+        min_y = np.min(vertices[:, 1])
+        min_z = np.min(vertices[:, 2])
+        max_x = np.max(vertices[:, 0])
+        max_y = np.max(vertices[:, 1])
+        max_z = np.max(vertices[:, 2])
         return [
             [min_x, min_y, min_z],
             [max_x, max_y, max_z],
@@ -148,18 +142,27 @@ if __name__ == "__main__":
     render = Render3D()
     mesh = Mesh()
     new_map = Map(
-        start_coord=[25.203884, 55.265894],
-        end_coord=[25.192079, 55.270721],
+        start_coord=[25.273621, 55.316846],
+        end_coord=[25.265242, 55.333434],
     )
 
-    buildings = new_map.generate_building_faces()
+    # new_graph = FreeSpaceGraph(
+    #     x_resolution=30,
+    #     y_resolution=30,
+    #     z_steps=5,
+    #     building_map=new_map,
+    #     nogo_zones=[0],  # in meters
+    # )
+    # path = new_graph.determine_shortest_path()
+    # print(path)
+
+    buildings: list[Building] = new_map.generate_building_faces()
     for building in tqdm(buildings):
-        counter = 1
-        for face in building:
-            mesh.add_2d_polygon(face)
-    vertices, faces = mesh.get_data()
+        mesh.add_box(building.footprint, building.height)
+
+    vertices, faces = mesh.get_vertices_faces()
     bounding_box = mesh.get_bounding_box()
 
     render.create_triangular_mesh(vertices, faces)
-    render.create_floor_mesh(bounding_box)
+    # render.create_floor_mesh(bounding_box)
     render.visualize()
